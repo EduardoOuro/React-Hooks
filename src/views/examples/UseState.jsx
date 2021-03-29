26ed8a3a8efe48c468bd24a1691fc02312a7d18d
Luis Conrado <luis.conrado@VALEMOBI.CORP@VMDESK-76.valemobi.corp>
2021-03-29 10:07:03 -0300

    implementados exemplos de useState exercicios 01 e 02

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState } from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
 
 const UseState = (props) => {
+
+    const [state, setState] = useState(0);
+    const [name, setName] = useState("");
+
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
+                subtitle="Estado em componentes funcionais!"/>
+
+            <SectionTitle title="Exercício #01"/>  
+            <div className="center">
+                <span className="text">{state}</span>
+                <div >
+                <button className="btn"
+                    onClick={()=> setState(state -1)}>-1</button>
+                <button className="btn"
+                    onClick={()=> setState(state +1)}>+1</button>
+                    <button className="btn"
+                    onClick={()=> setState(current => current +1000)}>+1000</button>
+                </div>
+                
+            </div>  
+            <SectionTitle title="Exercício #02"/>   
+            <input type="text" className="input"
+                value={name} onChange={event => setName(event.target.value)}/> 
+                <span className="text">{name}</span>
         </div>
     )
 }
