8cefe4ca14305d7bec1ce42d22d4b98154d59976
Luis Conrado <luis.conrado@VALEMOBI.CORP@VMDESK-76.valemobi.corp>
2021-03-29 10:33:30 -0300

    implementados exemplos de useEfect exercicios 01 e desafio 02

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useEffect, useState } from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
+
+function calcFatorial(num) {
+    const n = parseInt(num);
+    if (n < 0) return -1
+    if (n === 0) return 1
+    return calcFatorial(n - 1) * n;
+
+}
 
 const UseEffect = (props) => {
+
+    const [state, setState] = useState(1);
+    const [fatorial, setFatorial] = useState(1);
+
+    const [status, setStatus] = useState("ímpar");
+
+    useEffect(function () {
+        setFatorial(calcFatorial(state))
+    }, [state]);
+
+    useEffect(function () {
+        if (fatorial > 1000) {
+            document.title = "fatorial muito longo"
+        }
+    }, [fatorial]);
+
+    useEffect(function () {
+        setStatus(state % 2 === 0 ? "par" : "ímpar");
+    }, [state]);
+
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
+                subtitle="Permite executar efeitos colaterais em componentes funcionais!" />
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+                <div>
+                    <span className="text">Fatorial: </span>
+                    <span className="text red">{fatorial}</span>
+                </div>
+                <input type="number" className="input"
+                    value={state} onChange={e => setState(e.target.value)} />
+            </div>
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <div>
+                    <span className="text">Fatorial: </span>
+                    <span className="text red">{status}</span>
+                </div>
+            </div>
+
         </div>
     )
 }
